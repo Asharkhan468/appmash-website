db4c9029483885c77faea9d5cc6e48f57ba920c4
Asharkhan468 <asharullah.khan2468@gmail.com>
2025-08-15 18:51:11 +0500

    Config add

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    optimizeCss: false // Disables CSS optimization including PurgeCSS
+  }
 };
 
 export default nextConfig;
