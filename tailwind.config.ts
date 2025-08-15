71a8eefa8755c7efa6bdc94ff4a1def035e3dfc3
Asharkhan468 <asharullah.khan2468@gmail.com>
2025-08-15 18:33:54 +0500

    Config add

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,43 +1,40 @@
 // tailwind.config.ts
+import type { Config } from 'tailwindcss'
+
 const config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: {
+    files: [
+      './pages/**/*.{js,ts,jsx,tsx,mdx}',
+      './components/**/*.{js,ts,jsx,tsx,mdx}',
+      './app/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    // @ts-ignore  // TS warning ignore
+    safelist: [
+      'md:grid',
+      'md:grid-cols-2',
+      'order-1',
+      'order-2',
+      'text-center',
+      'md:text-left',
+      'justify-center',
+      'md:justify-start',
+      'space-y-4',
+      'gap-8',
+      'flex',
+      'items-center'
+    ],
+  },
   theme: {
     extend: {
       colors: {
-        primary1: "#d0363c",
-        primary2: "#b32724",
-        secondary1: "#ffffff",
-        secondary2: "#f9f9f9",
+        primary1: '#d0363c',
+        primary2: '#b32724',
+        secondary1: '#ffffff',
+        secondary2: '#f9f9f9',
       },
     },
   },
   plugins: [],
-  safelist: [
-    { pattern: /grid-cols-.*/ },
-    { pattern: /order-.*/ },
-    { pattern: /flex/ },
-    { pattern: /items-center/ },
-    { pattern: /justify-center/ },
-    { pattern: /min-h-.*/ },
-    { pattern: /bg-.*/ },
-    { pattern: /text-.*/ },
-     "md:grid",
-    "md:grid-cols-2",
-    "order-1",
-    "order-2",
-    "text-center",
-    "md:text-left",
-    "justify-center",
-    "md:justify-start",
-    "space-y-4",
-    "gap-8",
-    "flex",
-    "items-center"
-  ],
-};
+} satisfies Config
 
-export default config;
+export default config
